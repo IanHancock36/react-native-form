eda2cae7a7e55670dd4f4040557453e1aa1adb81
Ian Hancock <northroadian@gmail.com>
2022-08-19 10:26:45 -0500

    submit

--- screens/check-list-form.js
+++ screens/check-list-form.js
@@ -10,15 +10,40 @@ export const CheckListForm = () => {
     function toggle(value) {
         return !value;
     }
-    // OnPress for Log day needs condition to send alert if not all boxes are checked all must be checked before
-    // submission 
+
+    // const submitTheDay = () => {
+    //     if (!checkedTask && text) {
+    //         Alert.alert("You Did it! ")
+    //     } else {
+    //         Alert.alert("finish filling this form out")
+    //     }
+    // }
+
     const submitTheDay = () => {
-        if (!checkedTask && text) {
-            Alert.alert("You Did it! ")
-        } else {
-            Alert.alert("finish filling this form out")
-        }
-    }
+
+
+        db.collection("daily-check-list")
+            .add({
+                checkedTask: checkedTask,
+                text: text,
+            })
+            .then(() => {
+                Alert.alert("Your Day has been Logged!");
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        setCheckedTask(false);
+        onChangeText("");
+    };
+
+
+
+
+
+
+
+
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center", }}>
             <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
@@ -29,7 +54,7 @@ export const CheckListForm = () => {
                     <BouncyCheckbox style={{ marginVertical: 6 }} onPress={() => setCheckedTask(toggle)} />
                     <BouncyCheckbox style={{ marginVertical: 6 }} onPress={() => setCheckedTask(toggle)} />
                     <BouncyCheckbox style={{ marginVertical: 6 }} onPress={() => setCheckedTask(toggle)} />
-                    </View>
+                </View>
                 <View>
                     <Text style={{ marginVertical: 10 }}>1. Spiritual </Text>
                     <Text style={{ marginVertical: 10 }}>2. Mental </Text>
